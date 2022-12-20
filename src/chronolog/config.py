d57a175ed7809b377f6bfff734ac488a3ff9941b
jacobmiller22 <jacobmiller22@vt.edu>
2022-12-20 10:52:57 -0500

    Refactor to cli. Great start. Chronolog module now main module

--- src/chronolog/config.py
+++ src/chronolog/config.py
@@ -1,5 +1,4 @@
 import json
-import os
 
 
 class Config:
@@ -7,25 +6,34 @@ class Config:
     _config = None
     _path = None
 
-    def __init__(self, path_to_config):
-        self._path = path_to_config
-        self.__load_config()
+    def __init__(self, path):
+        """Initializes the Config class. Loads the provided config file.
 
-    def __load_config(self):
+        Args:
+            path (str): The path to the config file.
 
-        if not os.path.exists(self._path):
-            raise FileNotFoundError("Config file not found")
+        Raises:
+            FileNotFoundError: If the config file is not found.
+            ValueError: If the config file is not valid JSON.
+        """
+        self.__load_config(path)
 
-        with open(self._path, 'r') as f:
-            self._config = json.load(f)
+    def __load_config(self, path):
+        try:
+            with open(path, "r") as f:
+                self._config = json.load(f)
+        except FileNotFoundError as file_not_found_error:
+            raise FileNotFoundError("Config file not found") from file_not_found_error
+        except json.decoder.JSONDecodeError as json_value_error:
+            raise ValueError("Config file is not valid JSON") from json_value_error
 
-        # Parse config and make sure it's valid
+        self._path = path
 
     def get(self, key, default=None):
         """Returns the config as a dict"""
         if self._config is None:
             raise ValueError("Config not loaded")
-        split_key = key.split('.')
+        split_key = key.split(".")
         current = self._config
         for k in split_key:
             if k not in current:
@@ -37,7 +45,7 @@ class Config:
         """Updates the config"""
         if self._config is None:
             raise ValueError("Config not loaded")
-        split_key = key.split('.')
+        split_key = key.split(".")
         current = self._config
         for k in split_key[:-1]:
             current = current[k]
@@ -46,9 +54,20 @@ class Config:
         # Save the config
         self.__save()
 
+    def set_config(self, new_config: dict):
+        """Sets the config to the provided config.
+
+        Args:
+            new_config (dict): The new config.
+        """
+        self._config = new_config
+
+        # Save the config
+        self.__save()
+
     def __save(self):
-        """ Saves the current config object to the config file """
-        with open(self._path, 'w') as f:
+        """Saves the current config object to the config file"""
+        with open(self._path, "w") as f:
             json.dump(self._config, f, indent=2)
 
     def to_dict(self):
@@ -58,5 +77,5 @@ class Config:
         return self._config
 
     def get_path(self):
-        """ Returns the path to the config file """
+        """Returns the path to the config file"""
         return self._path
